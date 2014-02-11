537de7fb98f7c8fee06711dc8844742a4f76f2bc
Andrew Lin <rowlandking@gmail.com>
2014-02-11 14:57:49 -0800

    improvements on jquery and buttons

--- public/js/taskbook.js
+++ public/js/taskbook.js
@@ -74,3 +74,47 @@ function inputFocus(i){
 function inputBlur(i){
     if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
 }
+
+
+// Used
+function toggleEditGroup(name, name_Field, groupName) {
+    //do stuff
+    /*var x=document.forms["taskForm"]["tname"].value;
+    if (x==null || x=="" || x="New Task...") {
+        alert("Enter a task");
+        return false;
+    }*/
+    document.getElementById(name_Field).innerHTML = groupName;
+
+    if (document.getElementById(name).style.visibility=="visible") {
+        document.getElementById(name).style.visibility="hidden";
+    }
+    else document.getElementById(name).style.visibility="visible";
+
+
+
+    return true;
+}
+/*
+function addList(){
+    $("#grouplists").append("<div class='panel panel-primary'>
+  <div class='panel-body'>
+    NEW LIST
+  </div>
+  Add Task
+  <div class='panel-footer {{filters}}''>{{name}}</div>
+  {{/each}}
+  <a href='#'' onclick="">
+  <div id='add-{{name}}'class='panel-footer {{filters}}'style='text-align:right'>Add Task</div>
+  </a>
+</div>");
+
+}
+*/
+$("#addgrouplists").click(function(){
+     var html ='<div class="panel panel-primary">';
+        html +=' <div id = "'+'NEW LIST NAME HERE'+'"class="panel-body ">' +'NEW LIST NAME HERE'+ '</div>';
+        html +=' <div id="add-Name" class="panel-footer"style="text-align:right">Add Task</div></div>'
+  $("#grouplists").append(
+    html);
+});
