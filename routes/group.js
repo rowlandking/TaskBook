437f361a83865eaad9e4db8b0a324727ce9c2697
Andrew Lin <rowlandking@gmail.com>
2014-02-18 12:00:48 -0800

    extraction of json

--- routes/group.js
+++ routes/group.js
@@ -1,173 +1,181 @@
 exports.viewProject = function(req, res) {
   // controller code goes here 
+  console.log("group.js");
   var name = req.params.name;
-  console.log("The Group : " + name);
-  res.render('groups',{
-  	'projectName': name,
-  	   'groups': [
-      { 'name': 'CSE 170 Project',
-        'image': 'lorempixel.people.1.jpeg',
-        'id': 'project1'
+  var groupList = JSON.stringify([
+      { "name": "CSE 170 Project",
+        "image": "lorempixel.people.1.jpeg",
+        "id": "project1"
         
       },
-      { 'name': 'CSE 130',
-        'image': 'lorempixel.city.1.jpeg',
-        'id': 'project2'
+      { "name": "CSE 130",
+        "image": "lorempixel.city.1.jpeg",
+        "id": "project2"
       },
-      { 'name': 'Apartment 3449',
-        'image': 'lorempixel.technics.1.jpeg',
-        'id': 'project3'
+      { "name": "Apartment 3449",
+        "image": "lorempixel.technics.1.jpeg",
+        "id": "project3"
       },
-      { 'name': 'Book Club',
-        'image': 'lorempixel.abstract.1.jpeg',
-        'id': 'project4'
+      { "name": "Book Club",
+        "image": "lorempixel.abstract.1.jpeg",
+        "id": "project4"
       }
-    ],
-       'announcements': [
-      { 'type': 'Reminder',
-        'name': 'Buy Toilet Paper'
+    ]);
+   var announcementList =  JSON.stringify([
+      { "type": "Reminder",
+        "name": "Buy Toilet Paper"
       },
       { 
-        'type': 'Reminder',
-        'name': 'Clean Toilet'
+        "type": "Reminder",
+        "name": "Clean Toilet"
       }
-    ],
-    'contacts':[
+    ]);
+  var contactList = JSON.stringify([
           {
-            'name': 'Thuy Pham'
+            "name": "Thuy Pham"
           },
           {
-            'name': 'Carla Sun'
+            "name": "Carla Sun"
           },
           {
-            'name': 'Tim Pham'
+            "name": "Tim Pham"
           },
           {
-            'name': 'Thuy Tran'
+            "name": "Thuy Tran"
           },
           {
-            'name': 'Diana Pham'
+            "name": "Diana Pham"
           },
           {
-            'name': 'Marly Phung'
+            "name": "Marly Phung"
           },
           {
-            'name': 'David Phan'
+            "name": "David Phan"
           },
           {
-            'name': 'Troy Dam'
+            "name": "Troy Dam"
           }
 
-    ],
-    'lists':[
+    ]);
+  var listList = JSON.stringify([
     {
-      'id':'1',
-    	'name':'Chores',
-    	'tasks':[
-    	{
-    		'name':'Wash Dishes',
-        'filters': 'ready',
-        'id':'2'
-    	},
-  	  {
-    		'name':'Clean Toilet',
-        'filters': 'needsoon',
-         'id':'3'
+      "id":"1",
+      "name":"Chores",
+      "tasks":[
+      {
+        "name":"Wash Dishes",
+        "filters": "ready",
+        "id":"2"
+      },
+      {
+        "name":"Clean Toilet",
+        "filters": "needsoon",
+         "id":"3"
 
-    	},
-      {
-        'name':'Wipe Counters',
-        'filters': 'needsoon',
-        'id':'4'
       },
       {
-        'name':'Buy Toilet Paper',
-        'filters': 'urgent',
-        'id':'5'
+        "name":"Wipe Counters",
+        "filters": "needsoon",
+        "id":"4"
       },
       {
-        'name':'Get Eggs',
-        'filters': 'needsoon',
-        'id':'6'
+        "name":"Buy Toilet Paper",
+        "filters": "urgent",
+        "id":"5"
       },
       {
-        'name':'Buy Cheese',
-        'filters': 'ready',
-        'id':'7'
+        "name":"Get Eggs",
+        "filters": "needsoon",
+        "id":"6"
       },
       {
-        'name':'Find Apartment Key',
-        'filters': 'ready',
-        'id':'8'
+        "name":"Buy Cheese",
+        "filters": "ready",
+        "id":"7"
       },
       {
-        'name':'Walk Dog',
-        'filters':'needsoon',
-        'id':'9'
-      }
-    	]
-    },
-    {
-    'id':'2',
-		'name':'Schoolwork',
-    	'tasks':[
-    	{
-    		'name':'Turn in essay',
-        'name2':'Finish lab'
-    	},
-    	{
-    		'name':'Read Chapter 1',
-        'name2':'Read Chapter 1'
-    	},
-    	{
-    		'name':'Finish lab',
-        'name2':'Turn in essay'
-    	}
-    	]
-    },
-
-    ],
-    'fakelists':[
-    {
-      'id':'100',
-      'name':'Chores',
-      'tasks':[
-      {
-        'name':'Wash Dishes',
-        'filters': 'ready',
-        'id':'1000'
+        "name":"Find Apartment Key",
+        "filters": "ready",
+        "id":"8"
       },
       {
-        'name':'Take Out Trash',
-        'filters': 'ready',
-        'id':'1001'
-      },
-      {
-        'name':'Clean Dog',
-        'filters': 'ready',
-        'id':'1002'
+        "name":"Walk Dog",
+        "filters":"needsoon",
+        "id":"9"
       }
       ]
     },
     {
-    'id':'101',
-    'name':'Schoolwork',
-      'tasks':[
+    "id":"2",
+    "name":"Schoolwork",
+      "tasks":[
       {
-        'name':'Turn in essay',
-        'id':'1003'
+        "name":"Turn in essay",
+        "name2":"Finish lab"
       },
       {
-        'name':'Read Chapter 1',
-       'id':'1004'
+        "name":"Read Chapter 1",
+        "name2":"Read Chapter 1"
       },
       {
-        'name':'Finish lab',
-        'id':'1005'
+        "name":"Finish lab",
+        "name2":"Turn in essay"
       }
       ]
     },
 
-    ]
+    ]);
+  var fakelistList = JSON.stringify([
+    {
+      "id":"100",
+      "name":"Chores",
+      "tasks":[
+      {
+        "name":"Wash Dishes",
+        "filters": "ready",
+        "id":"1000"
+      },
+      {
+        "name":"Take Out Trash",
+        "filters": "ready",
+        "id":"1001"
+      },
+      {
+        "name":"Clean Dog",
+        "filters": "ready",
+        "id":"1002"
+      }
+      ]
+    },
+    {
+    "id":"101",
+    "name":"Schoolwork",
+      "tasks":[
+      {
+        "name":"Turn in essay",
+        "id":"1003"
+      },
+      {
+        "name":"Read Chapter 1",
+       "id":"1004"
+      },
+      {
+        "name":"Finish lab",
+        "id":"1005"
+      }
+      ]
+    }
+
+    ]);
+  console.log('The Group : ' + name);
+  console.log(JSON.parse(groupList));
+  console.log(fakelistList);
+  res.render('groups',{
+  	'projectName': name,
+  	'groups': JSON.parse(groupList),
+    'announcements':announcementList,
+    'contacts': JSON.parse(contactList),
+    'lists':JSON.parse(listList),
+    'fakelists': JSON.parse(fakelistList),
   });
 };
