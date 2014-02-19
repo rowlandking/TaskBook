f6a88ca3447c5d532889f43bd9c0090d8857b6a4
Andrew Lin <rowlandking@gmail.com>
2014-02-18 16:16:03 -0800

    cleaned code

--- public/js/taskbookLogin.js
+++ public/js/taskbookLogin.js
@@ -72,31 +72,6 @@ function signup(){
         }
     });*/
 }
-function getCookie(cname)
-{
-var name = cname + "=";
-var ca = document.cookie.split(';');
-for(var i=0; i<ca.length; i++) 
-  {
-  var c = ca[i].trim();
-  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-  }
-return "";
-}
-function checkCookie()
-{
-var username=getCookie("TBuserID");
-if (username!="")
-  {
-  //alert("Welcome again " + username);
-  console.log("Welcome Again" + username);
-  return true;
-  }
-else 
-  {
-  return false;
-  }
-}
 
 function SignupForm(){
     if(validateEmail()){
