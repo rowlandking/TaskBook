35a5a72551a3481ae98b5a8a047d05c9651da850
vagrant <karen.liyu.wu@gmail.com>
2014-02-20 07:41:12 +0000

    contactmodel

--- routes/contactmodel.js
+++ routes/contactmodel.js
@@ -34,7 +34,7 @@ exports.contactExists = function(req, res)
 		//console.log("no data");
 		//data_ = data.email;
 		if(error) console.log(error);
-    	res.send(data);
+    	res.json(data);
 
 	});
 	console.log("found something");
