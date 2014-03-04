a211c93349ad2aa765a85d43e9572432ef3f2726
vagrant <karen.liyu.wu@gmail.com>
2014-03-04 04:28:17 +0000

    added some more analytics stuff

--- public/js/taskbook.js
+++ public/js/taskbook.js
@@ -142,7 +142,7 @@ function toggleEditGroup(name, name_Field, groupName) {
         alert("Enter a task");
         return false;
     }*/
-
+    logClick();
     document.getElementById(name_Field).innerHTML = groupName;
 /*
     if (document.getElementById(name).style.visibility=="visible") {
@@ -795,3 +795,16 @@ $("#dueDate").click(function(){
   
 });
 $('.datepicker').datepicker();
+
+
+//google analytics code
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+
+function logClick(){
+  ga("send", "event", "button", "click");
+}
